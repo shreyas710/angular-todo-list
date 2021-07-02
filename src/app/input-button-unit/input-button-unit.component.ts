498cec7e463c43be2020c8925783f611cd235121
shreyas710 <shreyasnk5321@gmail.com>
2021-07-02 13:05:42 +0530

    @Input()

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -2,7 +2,12 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-input-button-unit',
-  templateUrl: './input-button-unit.component.html',
+  template: `
+    {{title}}
+    <input #inputElementRef [value]="title" (keyup.enter)="changeTitle($any($event.target).value)">
+
+    <button (click)="changeTitle(inputElementRef.value)">Save</button>
+  `,
   styleUrls: ['./input-button-unit.component.scss']
 })
 export class InputButtonUnitComponent implements OnInit {

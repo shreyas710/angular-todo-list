f0874a3e841759ecbd55d5c3d72b3216a94bc63c
shreyas710 <shreyasnk5321@gmail.com>
2021-07-02 13:25:39 +0530

    refractor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,27 +7,11 @@ import { TodoItem } from './interfaces/todo-item';
   <h1>
     Welcome to {{ title | uppercase }}!
   </h1>
-  <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
-  <ul>
-    <li *ngFor="let todoItem of todoList">
-      <app-todo-item [item]=todoItem></app-todo-item>
-    </li>
-  </ul>
+  <app-list-manager></app-list-manager>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'todo-list';
-  todoList: TodoItem[] = [
-    { title: 'install NodeJS' },
-    { title: 'install Angular CLI' },
-    { title: 'create new app' },
-    { title: 'serve app' },
-    { title: 'develop app' },
-    { title: 'deploy app' },
-  ];
 
-  addItem(title: string) {
-    this.todoList.push({ title });
-  }
 }
